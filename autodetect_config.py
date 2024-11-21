adcd8842f00a1187fe03d6ddc0825e88e50e9867
Blanca Fuentes Monjas <bfuentes@daint104.login.cscs.ch>
2024-11-21 17:23:46 +0100

    Final changes for asynchronous policy

--- autodetect_config.py
+++ autodetect_config.py
@@ -1,10 +1,10 @@
 import reframe.core.config as config
 
 site_configuration = config.detect_config(
-    exclude_feats=['c*-*', 'row*', 'contbuild', 'startx', 'group*', '128*'],
+    exclude_feats=[],
     # exclude_feats=[],
     detect_containers=True,
-    sched_options=['-A csstaff'],
+    sched_options=[],
     time_limit=200,
     filename='system_config'
 )
